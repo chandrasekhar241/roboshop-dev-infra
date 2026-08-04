62998f015d0936b9f492257c7e9c017055c3d299
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-04 19:36:08 +0530

    roboshop terraform

---
+++ 40-databases/iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "mysql" {
+  name = "${local.common_name}-mysql"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name}-mysql"
+    }
+  )
+}
+
+resource "aws_iam_policy" "mysql" {
+  name        = "${local.common_name}-mysql"
+  description = "Policy to read Mysql SSM parameter to attach to mysql instance"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = file("mysql-iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${local.common_name}-mysql"
+  role = aws_iam_role.mysql.name
+  
+}
