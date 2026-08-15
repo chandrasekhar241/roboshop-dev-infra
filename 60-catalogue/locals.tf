36a94c106f09743836d943e16921a96560d57cb5
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-15 19:12:53 +0530

    roboshop terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,7 +3,7 @@ locals {
   common_name = "${var.project}-${var.environment}"
   ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listener_arn
+  backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] #database-subnet-1a
   common_tags ={
     Project = "${var.project}"
