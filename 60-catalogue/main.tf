8d72053755596602799814e28b4e9d903e38bde7
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-15 16:41:54 +0530

    terraform roboshop

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "mongodb" {
+resource "aws_instance" "catalogue" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.catalogue_sg_id]
