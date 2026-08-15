68dc0c7af5f717f9cad844060278b87c1d0ec3b7
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-15 18:24:52 +0530

    roboshop terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -9,6 +9,11 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+    name = "/${var.project}/${var.environment}/vpc_id"
+  
+}
+
 data "aws_ami" "joindevops" {
   most_recent      = true
   owners           = ["973714476881"]
--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,6 +2,7 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   common_name = "${var.project}-${var.environment}"
   ami_id = data.aws_ami.joindevops.id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] #database-subnet-1a
   common_tags ={
     Project = "${var.project}"
