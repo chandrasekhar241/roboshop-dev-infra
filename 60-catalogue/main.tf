6201ada28505346823885c0eebdfb663498c1970
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-15 19:04:19 +0530

    roboshop terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -135,7 +135,7 @@ resource "aws_autoscaling_group" "catalogue" {
   force_delete              = false
   launch_template {
     id = aws_launch_template.catalogue.id
-    version = "latest"
+    version = "$Latest"
   }
   vpc_zone_identifier       = [local.private_subnet_id]
   target_group_arns = [aws_lb_target_group.catalogue.arn]
