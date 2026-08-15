d03351ee804e012830dc2dc9929b2e4d8b28f2ee
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-15 16:22:00 +0530

    roboshop terraform

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  common_name = "${var.project}-${var.environment}"
+  ami_id = data.aws_ami.joindevops.id
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] #database-subnet-1a
+  common_tags ={
+    Project = "${var.project}"
+    Environment ="${var.environment}"
+    Terraform = "true"
+  }
+
+}
+
---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+# variable "zone_id" {
+#     default = "Z08591453NE7S2K54TI4E"
+  
+# }
+
+# variable "domain_name" {
+#     default = "daws90saws.shop"
+  
+# }
