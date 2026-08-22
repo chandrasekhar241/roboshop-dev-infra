59ccb4760f465c8887095c386cf3e9bb2fc231aa
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-08-22 08:21:39 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "git::https://https://github.com/chandrasekhar241/terraform-roboshop-modules.git?ref=main"
+    source = "git::https://github.com/chandrasekhar241/terraform-roboshop-modules.git?ref=main"
     environment = var.environment
     component = each.key
      app_version = each.value.app_version
